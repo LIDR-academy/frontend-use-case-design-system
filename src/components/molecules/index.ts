6f5d401246d45f2256e1d780564d138ae7440d51
Earvin Saúl Pérez Ramos <sunde@MacBook-Air-de-Earvin.local>
2025-11-05 15:37:14 -0600

    agent add demo

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -3,3 +3,5 @@ export { Card } from './Card/Card';
 export type { CardProps } from './Card/Card';
 export { Tag } from './Tag/Tag';
 export type { TagProps } from './Tag/Tag';
+export { Select } from './Select/Select';
+export type { SelectProps, SelectOption } from './Select/Select';
